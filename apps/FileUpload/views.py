a739920d19fcd817ab328a3ef56ad556382b5578
[Zach.S] <[ZT.Stewart.5@gmail.com]>
2021-03-04 08:40:53 -0500

    Pre-save before adding file upload to project

--- apps/FileUpload/views.py
+++ apps/FileUpload/views.py
@@ -3,10 +3,9 @@ from apps.Login_Register.models import User
 from django.contrib import messages
 
 # Create your views here.
-def index(request):
-    return(render(request, "FileUpload/dashboard.html"))
-
 def dashboard(request):
     # if "user_id" not in request.session:
         # return(redirect("/ln"))
     return render(request, "FileUpload/dashboard.html")
+
+
